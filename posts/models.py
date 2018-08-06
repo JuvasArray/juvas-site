293000aaca38ed70008637aeef1ce4ab5089f560
Juvas Array <achouraithamiche40@gmail.com>
2018-08-06 19:42:37 +0100

    end of big stuffs, time to interesting details

--- posts/models.py
+++ posts/models.py
@@ -1,20 +1,31 @@
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.text import slugify
 
 
 def upload_location(instance, filename):
     return '%s/%s' % (instance.id, filename)
 
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length = 120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location, null=True, blank=True)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     created = models.DateTimeField(auto_now = False, auto_now_add = True)
     updated = models.DateTimeField(auto_now = True, auto_now_add = False)
+
+    objects = PostManager()
 
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,19 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404, HttpResponsePermanentRedirect, redirect
-
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, HttpResponsePermanentRedirect, redirect, Http404
+from django.utils import timezone
 
 from posts.models import Post
 from posts.forms import PostForm
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, 'Succeffuly created')
         return HttpResponsePermanentRedirect(instance.get_absolute_url())
@@ -20,6 +24,9 @@ def post_create(request):
 
 def post_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     context = {
             'title': instance.title,
             'instance': instance,
@@ -27,9 +34,17 @@ def post_detail(request, slug=None):
     return render(request, 'post_detail.html', context)
 
 def post_list(request):
-    queryset_list = Post.objects.all()
-    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+    queryset_list = Post.objects.active()
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
 
+    query = request.GET.get('query')
+    if query:
+        queryset_list = queryset_list.filter(Q(title__icontains=query) |
+               Q(content__icontains=query) | 
+               Q(user__first_name__icontains=query) |
+               Q(user__last_name__icontains=query) ).distinct()
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
 
     page = request.GET.get('page')
     try:
@@ -46,10 +61,13 @@ def post_list(request):
     return render(request, 'post_list.html', context)
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, 'Succeffuly updated')
         return HttpResponsePermanentRedirect(instance.get_absolute_url())
@@ -62,6 +80,8 @@ def post_update(request, slug=None):
     return render(request, 'post_form.html', context)
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, 'Succeffuly deleted')
